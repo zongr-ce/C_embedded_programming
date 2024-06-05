f6ed331d88d935731bcc1750cf8297ba48c932f2
Grace Zongo <zongraceo@gmail.com>
2024-06-05 14:22:59 -0500

    update of the sleep code

--- Sandbox/TX_RX commands with sleep mode atmega 2560/TB3216 Receive Control Commands/main.c
+++ Sandbox/TX_RX commands with sleep mode atmega 2560/TB3216 Receive Control Commands/main.c
@@ -9,14 +9,16 @@
 #define USART0_BAUD_RATE 9600
 #define LEDR_PIN PH6
 #define LEDG_PIN PH5
-#define int_SWITCH_PIN PD3  // Test-Status Switch
-#define pcint_SWITCH_PIN PE7 // MCU-Status Switch
+#define int_SWITCH_PIN PD3  // MCU switch
+#define sleep_switch_pin PE7// SD eject switch
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <string.h>
 #include <avr/sleep.h>
 #include <util/delay.h>
 
+void sys_disable(void);
+void sys_enable(void);
 void USART0_sendChar(char c);		// Declaration for sending characters to USART0
 void USART0_sendString(char *str);  // Declaration for sending Characters from a string to USART0_sendChar
 char USART0_read(void);				// Declaration for reading a character from the USART0
@@ -32,6 +34,72 @@ void INTERRUPTS_init(void);
 void USART0_init(void);
 void sleep_init(void);
 
+void sys_disable(){
+	DDRC |= (1 << DDC0) |(1 << DDC1) |(1 << DDC2) |(1 << DDC3) |(1 << DDC4) |(1 << DDC5) |(1 << DDC6) |(1 << DDC7);
+	DDRF |= (1 << DDF4) |(1 << DDF5) |(1 << DDF6);
+	DDRG |= (1 << DDG1) |(1 << DDG0);
+	DDRA |= (1 << DDA2) |(1 << DDA1) |(1 << DDA0);
+	DDRH |= (1 << DDH2) |(1 << DDH3) |(1 << DDH4);
+	
+	UCSR0B &= ~(1 << RXEN0);		// Disable RX for USART0
+	UCSR0B &= ~(1 << TXEN0);		// Disable TX for USART0
+	
+	
+	// DISABLE THE SDI12 INTERFACES
+	PORTH |= (1 << PH2);
+	PORTH &= ~(1 << PH3);
+	PORTH |= (1 << PH4);
+	
+	PORTA |= (1 << PA0);						// DISABLE_RS232_00
+	PORTA |= (1 << PA1);						// SHUTDOWN_RS232_00
+	PORTA &= ~(1 << PA2);						// SHUTDOWN_RS232_01_02 (TX MODULE)
+	PORTC &= ~(1 << PC0);						// DISABLE MODEM SWITCH
+	PORTC &= ~(1 << PC1);						// DISABLE 2.1V POWER
+	PORTC |= (1 << PC2);						// DISABLE TEMP_LOCAL initially enabled
+	PORTC |= (1 << PC3);						// DISABLE BARO
+	PORTC |= (1 << PC4);						// DISABLE WIND 01
+	PORTC |= (1 << PC5);						// DISABLE WIND 02
+	PORTC |= (1 << PC6);						// DISABLE VTRAN_01
+	PORTC &= ~(1 << PC7);						// DISABLE MUX
+	PORTG &= ~(1 << PG0);						// DISABLE VTRAN_03
+	PORTG &= ~(1 << PG1);						// DISABLE VTRAN_02
+	PORTF |= (1 << PF4);						// DISABLE ADC_01 +5V SWITCH
+	PORTF |= (1 << PF5);						// DISABLE ADC_02 +5V SWITCH
+	PORTF |= (1 << PF6);						// DISABLE ADC_03 +5V SWITCH
+}
+void sys_enable(){
+	DDRC |= (1 << DDC0) |(1 << DDC1) |(1 << DDC2) |(1 << DDC3) |(1 << DDC4) |(1 << DDC5) |(1 << DDC6) |(1 << DDC7);
+	DDRF |= (1 << DDF4) |(1 << DDF5) |(1 << DDF6);
+	DDRG |= (1 << DDG1) |(1 << DDG0);
+	DDRA |= (1 << DDA2) |(1 << DDA1) |(1 << DDA0);
+	DDRH |= (1 << DDH2) |(1 << DDH3) |(1 << DDH4);
+	
+	UCSR0B |= (1 << RXEN0);		// Disable RX for USART0
+	UCSR0B |= (1 << TXEN0);		// Disable TX for USART0
+	
+	// ENABLE THE SDI12 INTERFACES
+	PORTH &= ~(1 << PH2);
+	PORTH |= (1 << PH3);
+	PORTH &= ~(1 << PH4);
+	
+	PORTA &= ~(1 << PA0);						// ENABLE_RS232_00
+	PORTA &= ~(1 << PA1);						// DISABLE_SHUTDOWN_RS232_00
+	PORTA |= (1 << PA2);						// DISABLE_SHUTDOWN_RS232_01_02 (TX MODULE)
+	PORTC |= (1 << PC0);						// ENABLE MODEM SWITCH
+	PORTC |= (1 << PC1);						// ENABLE 2.1V POWER
+	PORTC &= ~(1 << PC2);						// ENABLE TEMP_LOCAL initially enabled
+	PORTC &= ~(1 << PC3);						// ENABLE BARO
+	PORTC &= ~(1 << PC4);						// ENABLE WIND 01
+	PORTC &= ~(1 << PC5);						// ENABLE WIND 02
+	PORTC &= ~(1 << PC6);						// ENABLE VTRAN_01
+	PORTC |= (1 << PC7);						// ENABLE MUX
+	PORTG |= (1 << PG0);						// ENABLE VTRAN_03
+	PORTG |= (1 << PG1);						// ENABLE VTRAN_02
+	PORTF &= ~(1 << PF4);						// ENABLE ADC_01 +5V SWITCH
+	PORTF &= ~(1 << PF5);						// ENABLE ADC_02 +5V SWITCH
+	PORTF &= ~(1 << PF6);						// ENABLE ADC_03 +5V SWITCH
+}
+
 void LED_init(void){
 	DDRH |= (1 << DDH6);		//Setting data direction for red LED to "output"
 	DDRH |= (1 << DDH5);		//Setting data direction for green LED to "output"
@@ -39,9 +107,8 @@ void LED_init(void){
 void SWITCH_init(void){
 	DDRD &= ~(1 << DDD3);
 	PORTD |= (1 << int_SWITCH_PIN); //Enabling PULL-UP on the switch for INT0
-	
 	DDRE &= ~(1 << DDE7);
-	PORTE |= (1 << pcint_SWITCH_PIN); //Enabling PULL-UP on the switch for PCINT0
+	PORTE |= (1 << sleep_switch_pin); //Enabling PULL-UP on the sleep switch
 }
 void USART0_init(void){
 	UBRR0 = ((float)(F_CPU / (16 * (float)USART0_BAUD_RATE)) - 1);
@@ -62,15 +129,7 @@ void INTERRUPTS_init(void){
 							// When changing the ISCn1/ISCn0 bits, the interrupt must be disabled by clearing its Interrupt Enable bit
 	EICRA |= (1 << ISC31);	// in the EIMSK Register. Otherwise an interrupt can occur when the bits are changed.
 	
-	EIMSK |= (1 << INT3);	// enables interrupt 3 (INT6)
-	
-	// EIFR |= (1 << INTF0);	// Clearing the interrupt flag (The flag is also cleared when the interrupt routine is executed.)
-	
-	PCICR |= (1 << PCIE0);	// Enabling pin change interrupt 0 (PCICR0)
-	
-	PCMSK0 |= (1 << pcint_SWITCH_PIN); //enables the Pin Change Interrupt (PB4)
-	
-	sei();					// sets global interrupts by writing a 1 to the I-bit (interrupt-bit) in the Status Register (SREG)
+	EIMSK |= (1 << INT3);	// enables interrupt 3 (INT3)
 }
 void sleep_init(void){
 	/* SMCR &= ~(1 << SM0);		|
@@ -103,6 +162,7 @@ void blink_Leds(int x){
 	green_LED_ON();
 	red_LED_ON();
 }
+
 void USART0_sendChar(char c){
 	/*This next loop checks if the previous transmission is completed by checking the USARTn.STATUS register.
 	This is done by polling the status register until the empty flag "STATUS.DREIF" is set.
@@ -134,21 +194,20 @@ void execute(char *command)
 	else if (strcmp(command, "rOFF") == 0){
 		USART0_sendString("Red LED is now OFF.\r\n");
 		red_LED_OFF();
-		}else if (strcmp(command, "gON") == 0){
+	}else if (strcmp(command, "gON") == 0){
 		USART0_sendString("Green LED is now ON.\r\n");
 		green_LED_ON();
-		} else if (strcmp(command, "gOFF") == 0){
+	} else if (strcmp(command, "gOFF") == 0){
 		USART0_sendString("Green LED is now OFF.\r\n");
 		green_LED_OFF();
-		} else if (strcmp(command, "blink") == 0){
+	} else if (strcmp(command, "blink") == 0){
 		USART0_sendString("Green LED and Red LED are now blinking.\r\n");
 		blink_Leds(20);
-		} else if (strcmp(command, "sleep") == 0){
-		USART0_sendString("Device in sleep mode.\r\n");
-		blink_Leds(3);
-		red_LED_OFF();
-		green_LED_OFF();
-		sleep_mode();
+	} else if (strcmp(command, "sleep") == 0){		// here is where in invoke the sleep mode
+		red_LED_OFF();								// Turns off red LED
+		green_LED_OFF();							// Turns off green LED
+		sys_disable();								// disables all modules on the boards. can be changed to keep some modules on
+		sleep_mode(); // first sleep instruction needs to be invoked twice.
 		}else{
 		USART0_sendString("Your Command is undefined.\r\n");
 	}
@@ -157,15 +216,13 @@ void execute(char *command)
 ISR (INT3_vect){
 	if ((PIND & (1 << int_SWITCH_PIN))){
 		//sleep_disable();
-		USART0_sendString("Device is awake.\r\n");
-		blink_Leds(5);
-		green_LED_ON();
+		sys_enable();
 		red_LED_ON();
+		green_LED_ON();
 	}
 }
 
-int main(void)
-{
+int main(void){
 	
 	char command[8];
 	int index = 0;
@@ -175,8 +232,17 @@ int main(void)
 	LED_init();
 	SWITCH_init();
 	sleep_init();
+	sei();					// sets global interrupts by writing a 1 to the I-bit (interrupt-bit) in the Status Register (SREG)
+
     while (1) 
     {
+		
+	/*	if (!(PINE & (1 << sleep_switch_pin))){
+			red_LED_OFF();								// Turns off red LED
+			green_LED_OFF();							// Turns off green LED
+			sys_disable();								// disables all modules on the boards. can be changed to keep some modules on
+		}*/
+			
 		c = USART0_read();
 		if (c != '\n' && c !='\r'){
 			command[index++] = c;
